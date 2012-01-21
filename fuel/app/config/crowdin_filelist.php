154ae88c6701689ec491964e8466b18fbd66c730
kenjis <kenji.uui@gmail.com>
2012-01-21 11:29:37 +0900

    fix config/crowdin_filelist.php

--- fuel/app/config/crowdin_filelist.php
+++ fuel/app/config/crowdin_filelist.php
@@ -6,3 +6,12 @@
 * @copyright  2012 Kenji Suzuki
 * @license    MIT License http://www.opensource.org/licenses/mit-license.php
 */
+
+/**
+* Data of Files which are added to Crowdin
+*/
+return array(
+	'filelist_data' => 
+	array(
+	),
+);
